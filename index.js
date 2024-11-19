9764817011dadb25814cfe4c2db1a927456e6a7d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-11-19 21:13:01 +0700

    Create todo create route

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import express from "express";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "./firebase-config.js";
+import { Todo } from "./todo-schema.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+const todosCollection = collection(db, "todos");
+
+// Arrow Function
+app.post("/todos", async (req, res) => {
+  try {
+    const todo = new Todo(
+      req.body.author,
+      req.body.description,
+      req.body.title,
+    );
+
+    await addDoc(todosCollection, JSON.parse(JSON.stringify(todo)));
+
+    res.send("Document has been saved!");
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 });
 
 app.listen(port, () => {
